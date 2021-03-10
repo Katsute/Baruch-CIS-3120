bbf6dec43936e672e9e961468f02b2309b2dd6cd
Katsute <58778985+Katsute@users.noreply.github.com>
2021-03-09 21:57:05 -0500

    hw & proj.

--- assignments/HW2/HW2.py
+++ assignments/HW2/HW2.py
@@ -1,18 +1,41 @@
+from typing import List
+
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 from pandas import DataFrame
 
 
 def main() -> None:
+    # read site
+    url: str = "https://en.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States_by_population"
+    soup: BeautifulSoup = BeautifulSoup(requests.get(url).content, "html.parser")
 
-    # todo: parse site & export to CSV
+    [s.extract() for s in soup.find_all("sup", {"class": "reference"})]
 
+    # get table body
+    table: Tag = soup.find("table", {"class": "wikitable sortable", "style": "width:100%; text-align:center;"})
 
-    # todo: parse CSV to data frame
-    df: DataFrame = DataFrame()
+    # populate dataset
+    data: List[List[str or float]] = []
+    for row in table.find("tbody").find_all("tr"):  # for each row
+        row: Tag
+        if row.has_attr("class") and row["class"][0] == "sortbottom" or row.find("th"):  # exclude header and footer rows
+            continue
+        cols: List[str or float] = []
+        for col in row.find_all("td"):  # for each col
+            raw: str = col.text.strip()
+            try:  # if string can be expressed as float then cast, otherwise save raw string
+                cols.append(float(raw.strip("+%*").replace(',', '').replace("–", '-')))
+            except ValueError:
+                cols.append(raw)
+        data.append(cols)
 
+    # export as csv DataFrame
+    df: DataFrame = DataFrame(data, columns=["Current Rank", "2010 Rank", "State/Territory", "2020 Pop.", "2010 Pop.", "% Pop. Change", "Net Change", "House Seats", "Percent House", "Pop. / Electoral Vote", "2020 Pop. / Seat", "2010 Pop. / Seat", "2020 % US Pop.", "2010 % US Pop.", "% US Pop. Change", "% Electoral College"])
+    df.to_csv("us_pop.csv")
 
-    print(df.describe())
+    # describe
+    print(df[["2020 Pop.", "2020 % US Pop."]].describe())
 
     return
 
--- assignments/Project1/Project1.py
+++ assignments/Project1/Project1.py
@@ -1,45 +1,53 @@
 from typing import Dict, Callable, List
 
 import requests
-from bs4 import BeautifulSoup, Tag
+from bs4 import BeautifulSoup
 from pandas import DataFrame
 
 '''
 Methods to parse string as a float height.
 '''
 
+
 # parse 0'0"
-parse_qt_height: Callable[[str], float] = lambda x: int(x[0:x.index('\'')]) + (float(x[x.index['\''] + 1 : -1]) / 12)
+def parse_qt_height(raw: str) -> float:
+    return int(raw[0:raw.index('\'')]) + (float(raw[raw.index('\'') + 1 : -1]) / 12)
+
+
 # parse 0-0
-parse_ln_height: Callable[[str], float] = lambda x: int(x[0:x.index['-']]) + (float(x[x.index['-'] + 1]) / 12)
+def parse_ln_height(raw: str) -> float:
+    return int(raw[0:raw.index('-')]) + (float(raw[raw.index('-') + 1]) / 12)
+
 
 '''
-List of methods used to parse soup to names and heights.
+List of methods used to parse soup as names and heights from various sites.
 '''
 
-# <row,name,height,parse_height>
-parse_bkln: List[Callable] = [None, None, None, parse_qt_height]
-parse_brch: List[Callable] = [None, None, None, parse_qt_height]
-parse_york: List[Callable] = [None, None, None, parse_ln_height]
-parse_jjay: List[Callable] = [None, None, None, parse_qt_height]
-parse_qc: List[Callable]   = [None, None, None, parse_ln_height]
+
+def parse_list(soup: BeautifulSoup) -> Dict[str, float]:
+    return {row.find("h3").text.strip(): parse_qt_height(row.find("span", {"class": "sidearm-roster-player-height"}).text) for row in soup.find_all("div", {"class": "sidearm-roster-player-pertinents"})}
+
+
+def parse_table(soup: BeautifulSoup) -> Dict[str, float]:
+    return {row.find("td", {"class": "sidearm-table-player-name"}).text.strip(): parse_ln_height(row.find("td", {"class": "height"}).text) for row in soup.find("div", {"data-bind": "if: active_template().id == 2, css: {'sidearm-roster-template-active': active_template().id == 2}"}).find("tbody").find_all("tr")}
+
 
 '''
 Team URLs and required parsing method (above).
 '''
-urls: Dict[str, List[Callable]] = {
-    "https://www.brooklyncollegeathletics.com/sports/mens-volleyball/roster/2019": parse_bkln,
-    "https://www.brooklyncollegeathletics.com/sports/womens-volleyball/roster/2019": parse_bkln,
-    "https://athletics.baruch.cuny.edu/sports/mens-volleyball/roster": parse_brch,
-    "https://athletics.baruch.cuny.edu/sports/womens-volleyball/roster": parse_brch,
-    "https://yorkathletics.com/sports/mens-volleyball/roster": parse_york,
-    "https://johnjayathletics.com/sports/womens-volleyball/roster": parse_jjay,
-    "https://www.brooklyncollegeathletics.com/sports/mens-swimming-and-diving/roster": parse_bkln,
-    "https://www.brooklyncollegeathletics.com/sports/womens-swimming-and-diving/roster": parse_bkln,
-    "https://athletics.baruch.cuny.edu/sports/mens-swimming-and-diving/roster": parse_brch,
-    "https://athletics.baruch.cuny.edu/sports/womens-swimming-and-diving/roster": parse_brch,
-    "https://yorkathletics.com/sports/mens-swimming-and-diving/roster": parse_york,
-    "https://queensknights.com/sports/womens-swimming-and-diving/roster": parse_qc
+urls: Dict[str, List[Callable[[BeautifulSoup], Dict[str, float]]]] = {
+    "https://www.brooklyncollegeathletics.com/sports/mens-volleyball/roster/2019": parse_list,
+    "https://www.brooklyncollegeathletics.com/sports/womens-volleyball/roster/2019": parse_list,
+    "https://athletics.baruch.cuny.edu/sports/mens-volleyball/roster": parse_list,
+    "https://athletics.baruch.cuny.edu/sports/womens-volleyball/roster": parse_list,
+    "https://yorkathletics.com/sports/mens-volleyball/roster": parse_table,
+    "https://johnjayathletics.com/sports/womens-volleyball/roster": parse_list,
+    "https://www.brooklyncollegeathletics.com/sports/mens-swimming-and-diving/roster": parse_list,
+    "https://www.brooklyncollegeathletics.com/sports/womens-swimming-and-diving/roster": parse_list,
+    "https://athletics.baruch.cuny.edu/sports/mens-swimming-and-diving/roster": parse_list,
+    "https://athletics.baruch.cuny.edu/sports/womens-swimming-and-diving/roster": parse_list,
+    "https://yorkathletics.com/sports/mens-swimming-and-diving/roster": parse_table,
+    "https://queensknights.com/sports/womens-swimming-and-diving/roster": parse_table
 }
 
 mens_swimming: DataFrame
@@ -51,12 +59,17 @@ womens_volleyball: DataFrame
 def q1() -> None:
     print("1. Scrape data and compile a dataframe of all the names and heights of the players on the men’s swimming team")
 
-    urlset: Dict[str, List[Callable]] = {url: v for url, v in urls.items() if "mens-swimming" in url}
+    # get urls
+    url_set: Dict[str, Callable[[BeautifulSoup], Dict[str, float]]] = {url: v for url, v in urls.items() if "/mens-swimming" in url}
 
     df: DataFrame = DataFrame()
 
-    # for each url run associated methods and condense into a single dataframe
-    [df.append(get_heights(url, method[0], method[1], method[2], method[3])) for url, method in urlset.items()]
+    # for each url get names and heights then condense into a single dataframe
+    global mens_swimming
+    for url, method in url_set.items():
+        df = df.append(get_heights(url, method))
+    df = df.sort_values(by="height")
+    mens_swimming = df
 
     print(df)
 
@@ -66,12 +79,17 @@ def q1() -> None:
 def q2() -> None:
     print("2. Scrape data and compile a dataframe of all the names and heights of the players on the women’s swimming team")
 
-    urlset: Dict[str, List[Callable]] = {url: v for url, v in urls.items() if "womens-swimming" in url}
+    # get urls
+    url_set: Dict[str, Callable[[BeautifulSoup], Dict[str, float]]] = {url: v for url, v in urls.items() if "/womens-swimming" in url}
 
     df: DataFrame = DataFrame()
 
-    # for each url run associated methods and condense into a single dataframe
-    [df.append(get_heights(url, method[0], method[1], method[2], method[3])) for url, method in urlset.items()]
+    # for each url get names and heights then condense into a single dataframe
+    global womens_swimming
+    for url, method in url_set.items():
+        df = df.append(get_heights(url, method))
+    df = df.sort_values(by="height")
+    womens_swimming = df
 
     print(df)
 
@@ -81,12 +99,17 @@ def q2() -> None:
 def q3() -> None:
     print("3. Scrape data and compile a dataframe of all the names and heights of the players on the men’s volleyball team")
 
-    urlset: Dict[str, List[Callable]] = {url: v for url, v in urls.items() if "mens-volleyball" in url}
+    # get urls
+    url_set: Dict[str, Callable[[BeautifulSoup], Dict[str, float]]] = {url: v for url, v in urls.items() if "/mens-volleyball" in url}
 
     df: DataFrame = DataFrame()
 
-    # for each url run associated methods and condense into a single dataframe
-    [df.append(get_heights(url, method[0], method[1], method[2], method[3])) for url, method in urlset.items()]
+    # for each url get names and heights then condense into a single dataframe
+    global mens_volleyball
+    for url, method in url_set.items():
+        df = df.append(get_heights(url, method))
+    df = df.sort_values(by="height")
+    mens_volleyball = df
 
     print(df)
 
@@ -96,12 +119,17 @@ def q3() -> None:
 def q4() -> None:
     print("4. Scrape data and compile a dataframe of all the names and heights of the players on the women’s volleyball team")
 
-    urlset: Dict[str, List[Callable]] = {url: v for url, v in urls.items() if "womens-volleyball" in url}
+    # get urls
+    url_set: Dict[str, Callable[[BeautifulSoup], Dict[str, float]]] = {url: v for url, v in urls.items() if "/womens-volleyball" in url}
 
     df: DataFrame = DataFrame()
 
-    # for each url run associated methods and condense into a single dataframe
-    [df.append(get_heights(url, method[0], method[1], method[2], method[3])) for url, method in urlset.items()]
+    # for each url get names and heights then condense into a single dataframe
+    global womens_volleyball
+    for url, method in url_set.items():
+        df = df.append(get_heights(url, method))
+    df = df.sort_values(by="height")
+    womens_volleyball = df
 
     print(df)
 
@@ -110,33 +138,25 @@ def q4() -> None:
 
 def get_heights(
         url: str,
-        soup_row: Callable[[BeautifulSoup], Tag],
-        soup_name: Callable[[Tag], str],
-        soup_height: Callable[[Tag], str],
-        parse_height: Callable[[str], float]) -> DataFrame:
-    """
-    Parses heights from a site into a dataframe.
-
-    :param url: site url
-    :param soup_row: method that retrieves person rows from soup
-    :param soup_name: method that retrieves name from person row
-    :param soup_height: method that retrieves height from person row
-    :param parse_height: method that parses height syntax
-    :return:
-    """
+        parse: Callable[[BeautifulSoup], Dict[str, float]]) -> DataFrame:
     soup: BeautifulSoup = BeautifulSoup(requests.get(url).content, "html.parser")
-    # for each row in the table, extract the name and height to populate a dictionary
-    data: Dict[str, float] = {soup_name(tag): parse_height(soup_height(tag)) for tag in soup_row(soup)}
+    # for each row in the table, extract the row then extract the name and height to populate a dictionary
+    data: Dict[str, float] = parse(soup)
 
-    return DataFrame(data)
+    return DataFrame({"name": data.keys(), "height": data.values()})
 
 
 def q5() -> None:
     print("5. Find the average height in each of the 4 dataframes (so you should have 4 averages in total)")
 
-    if not mens_volleyball or not womens_volleyball or not mens_swimming or not womens_swimming:
+    if mens_volleyball is None or womens_volleyball is None or mens_swimming is None or womens_swimming is None:
         print("Please run q1(), q2(), q3(), and q4()")
         return
+
+    print(f"Men's Volleyball Average: {mens_volleyball['height'].mean():.2f} ft.")
+    print(f"Men's Swimming Average: {mens_swimming['height'].mean():.2f} ft.")
+    print(f"Women's Volleyball Average: {womens_volleyball['height'].mean():.2f} ft.")
+    print(f"Women's Swimming Average: {womens_swimming['height'].mean():.2f} ft.")
 
     return
 
@@ -144,9 +164,18 @@ def q5() -> None:
 def q6() -> None:
     print("6. List the names and the heights of the 5 tallest and the 5 shortest swimmers and volleyball players for both the men’s and women’s teams. That is you must have 8 lists in total: tallest men swimmers, tallest men volleyball players, tallest women swimmers, tallest women volleyball players, shortest men swimmers, shortest women volleyball players, shortest women swimmers, shortest women volleyball players,")
 
-    if not mens_volleyball or not womens_volleyball or not mens_swimming or not womens_swimming:
+    if mens_volleyball is None or womens_volleyball is None or mens_swimming is None or womens_swimming is None:
         print("Please run q1(), q2(), q3(), and q4()")
         return
+
+    print("Men's Volleyball (shortest)"     , mens_volleyball.head(5)  , sep='\n')
+    print("Men's Volleyball (tallest)"      , mens_volleyball.tail(5)  , sep='\n')
+    print("Men's Swimming (shortest)"       , mens_swimming.head(5)    , sep='\n')
+    print("Men's Swimming (tallest)"        , mens_swimming.tail(5)    , sep='\n')
+    print("Women's Volleyball (shortest)"   , womens_volleyball.head(5), sep='\n')
+    print("Women's Volleyball (tallest)"    , womens_volleyball.tail(5), sep='\n')
+    print("Women's Swimming (shortest)"     , womens_swimming.head(5)  , sep='\n')
+    print("Women's Swimming (tallest)"      , womens_swimming.tail(5)  , sep='\n')
 
     return
 
