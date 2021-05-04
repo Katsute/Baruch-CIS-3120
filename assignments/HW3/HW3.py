b61dc752027f4eea1f588144fef521cfea055a95
Katsute <58778985+Katsute@users.noreply.github.com>
2021-05-04 16:03:31 -0400

    3

--- assignments/HW3/HW3.py
+++ assignments/HW3/HW3.py
@@ -1,26 +1,44 @@
-import sys
 import urllib.parse
-from typing import Dict, Any
+from typing import Dict, List
 
+import pandas as pd
 import requests
 
-def main(*args: [Any]) -> None:
-    if not args[0]:
-        return
+
+# API: https://docs.coincap.io/
+# API requires no token
+def main() -> None:
+    base_url: str = "https://api.coincap.io/v2"
+
+    currencies: List[str] = ["bitcoin", "bitcoin-cash", "binance-coin", "dogecoin", "ethereum", "ethereum-classic", "litecoin", "monero", "stellar", "tether"]
+
+    dataset: Dict[str, Dict[str, float]] = {}
+
+    for c in currencies:
+        data: any = request(base_url, "assets/" + c)["data"]
+        dataset[c] = {
+            "supply":        float(data["supply"] or 0),
+            "maxSupply":     float(data["maxSupply"] or 0),
+            "marketCapUsd":  float(data["marketCapUsd"] or 0),
+            "volumeUsd24Hr": float(data["volumeUsd24Hr"] or 0),
+            "priceUsd":      float(data["priceUsd"] or 0)
+        }
+
+    pd.options.display.width = None
+    pd.options.display.float_format = lambda x: '%.2f' % x
+    df: pd.DataFrame = pd.DataFrame(dataset)
+    print(df)
+
+    df.to_csv("crypto.csv")
+
     return
 
 
-def request(path: str, **kwargs: Dict[str, Any]) -> Any:
-    url: str = path
-    first: bool = True
-    for k, v in kwargs:
-        url += '?' if first else '&'
-        first = False
-        url += f"{urllib.parse.quote_plus(k)}={urllib.parse.quote_plus(v)}"
-
+def request(base_url: str, path: str) -> any:
+    url: str = base_url + '/' + path
     response: requests.Response = requests.get(url)
     return response.json()
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
--- assignments/Project2/Project2.py
+++ assignments/Project2/Project2.py
@@ -1,19 +1,74 @@
-import sys
+import time
 import urllib.parse
-from typing import Dict, Any
+from typing import Dict, List
 
+from bs4 import BeautifulSoup, Tag
+import pandas as pd
 import requests
 
-def main(*args: [Any]) -> None:
-    if not args[0]:
-        return
+
+# API: https://docs.coincap.io/
+# API requires no token
+# Table: https://coinmarketcap.com/all/views/all/
+def main() -> None:
+    # Table scrape
+    url: str = "https://coinmarketcap.com/all/views/all"
+    soup: BeautifulSoup = BeautifulSoup(requests.get(url).content, "html.parser")
+
+    i: int = 0
+    limit: int = 50  # max to process
+    tableset: List[List[str or float]] = []
+    for row in soup.find_all("tr", {"class": "cmc-table-row", "style": "display:table-row"}):  # for each row
+        i += 1
+        if i > limit: break
+
+        row: Tag
+        cols: List[str or float] = []
+        for col in row.find_all("td"):  # for each col
+            raw: str = col.text.strip()
+            try:  # if string can be expressed as float then cast, otherwise save raw string
+                cols.append(float(raw.strip("+%*").replace(',', '').replace("–", '-')))
+            except ValueError:
+                cols.append(raw)
+        tableset.append(cols)
+
+    tbl_df: pd.DataFrame = pd.DataFrame(tableset, columns=["Rank", "Name", "Symbol", "Market Cap", "Price", "Circulating Supply", "Volume(24h)", "% 1h", "% 24h", "% 7d", "..."])
+
+    # API
+    base_url: str = "https://api.coincap.io/v2"
+    dataset: Dict[str, Dict[str, float or str]] = {}
+
+    i: int = 0
+    size: int = len(tbl_df["Symbol"])
+    for c in tbl_df["Symbol"]:
+        i += 1
+        print(f"Processing {i}/{size}")
+        data: any = request(base_url, "assets/", search=c)["data"][0]
+        dataset[c] = {
+            "symbol": data["symbol"],
+            "supply": float(data["supply"] or 0),
+            "maxSupply": float(data["maxSupply"] or 0),
+            "marketCapUsd": float(data["marketCapUsd"] or 0),
+            "volumeUsd24Hr": float(data["volumeUsd24Hr"] or 0),
+            "priceUsd": float(data["priceUsd"] or 0)
+        }
+        time.sleep(1)  # prevent rate limit
+
+    api_df: pd.DataFrame = pd.DataFrame(dataset)
+
+    # merge df
+    df: pd.DataFrame = pd.merge(tbl_df, api_df, on=["Symbol", "symbol"])
+    print(df)
+
+    df.to_csv("crypto.csv")
+
     return
 
 
-def request(path: str, **kwargs: Dict[str, Any]) -> Any:
-    url: str = path
+def request(base_url: str, path: str, **kwargs: Dict[str, any]) -> any:
+    url: str = base_url + '/' + path
     first: bool = True
-    for k, v in kwargs:
+    for k, v in kwargs.items():  # query parameters
         url += '?' if first else '&'
         first = False
         url += f"{urllib.parse.quote_plus(k)}={urllib.parse.quote_plus(v)}"
@@ -23,4 +78,4 @@ def request(path: str, **kwargs: Dict[str, Any]) -> Any:
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
